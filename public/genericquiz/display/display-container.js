1af05170dc4dc48ae57db35431b4a8e16b452d38
Michael Doyle <MichaelTDoyle93@gmail.com>
2018-02-03 22:07:36 +1030

    Text scaling and animations in Generic Quiz displays

--- public/genericquiz/display/display-container.js
+++ public/genericquiz/display/display-container.js
@@ -2,6 +2,7 @@ var React = require("react");
 var PropTypes = require("prop-types");
 const io = require("socket.io-client");
 import PlayerListing from "./player-listing";
+import {Layer, Stage} from "react-konva";
 
 export default class DisplayContainer extends React.Component {
 	constructor(props) {
@@ -9,56 +10,57 @@ export default class DisplayContainer extends React.Component {
 		this.state = {
 			buzzersOpen: false,
 			players: [],
-
+			height: 0,
+			width: 0,
 			playerAnswering: {},
 		};
+	}
+
+	updateDimensions = () => {
+		this.setState({width: window.innerWidth, height: window.innerHeight});
 	}
 
 	onNewState = (state) => {
 		this.setState(state);
 	}
 
+	componentWillMount = () => {
+		this.updateDimensions();
+	}
+
 	componentDidMount = () => {
 		this.props.socket.on("new game state", this.onNewState);
+		window.addEventListener("resize", this.updateDimensions);
 	}
 
 	componentWillUnmount = () => {
 		this.props.socket.removeListener("new game state", this.onNewState);
+		window.removeEventListener("resize", this.updateDimensions);
 	}
 
 	render = () => {
-		var questionPanel;
-		var playerPanel;
-		const nonHiddenPlayers = this.state.players.filter((p) => {
-			return !p.hidden;
-		});
-		if (nonHiddenPlayers.length != 0) {
-			var list = nonHiddenPlayers.map((p, i) => {
-				// light this display up if they are answering the question
-				var answering = this.state.playerAnswering.screenName === p.screenName;
+		const {height, width, players, playerAnswering} = this.state;
+		const nonHiddenPlayers = players.filter(p => !p.hidden);
 
-				return (
-					<PlayerListing
-						player={p}
-						key={i}
-						answering={answering}
-					/>
-				);
-			});
-			playerPanel = <div className="playerContainer">{list}</div>;
-		} else {
-			playerPanel = <div className="playerContainer"/>;
-		}
+		const playerHeight = Math.min(0.1 * height, height / nonHiddenPlayers.length);
+		const firstPlayerTop = (height - (nonHiddenPlayers.length * playerHeight)) / 2;
+		const list = nonHiddenPlayers.map((p, i) => (
+			// light this display up if they are answering the question
+			<PlayerListing
+				player={p}
+				key={i}
+				height={playerHeight}
+				width={width}
+				top={firstPlayerTop + (i * playerHeight)}
+				answering={playerAnswering.screenName === p.screenName}
+			/>
+		));
+		const playerPanel = <Layer>{list}</Layer>;
 
 		return (
-			<div id="display-panel" className="content">
-				<div id="question-panel" className="content">
-					{questionPanel}
-				</div>		
-				<div id="player-list" className="content">
-					{playerPanel}
-				</div>
-			</div>
+			<Stage height={height} width={width}>
+				{playerPanel}
+			</Stage>
 		);
 	}
 }
--- public/genericquiz/display/player-listing.js
+++ public/genericquiz/display/player-listing.js
@@ -1,39 +1,89 @@
 var React = require("react");
 var PropTypes = require("prop-types");
+import {Text, Rect, Group} from "react-konva";
+import {Motion, spring} from "react-motion";
 
 export default class PlayerListing extends React.Component {
-	render = () => {
-		var scoreString = this.props.player.score;
+	constructor(props) {
+		super(props);
+	}
 
-		var className = "playerListingDetails";
+	textWidth = (text, font, size) => {
+		var c=document.createElement("canvas");
+		var cctx=c.getContext("2d");
+		cctx.font = size + "px " + font;
+		return cctx.measureText(text).width;
+	}
 
-		if (this.props.answering) {
-			return (
-				<div className="playerListing">
-					<div className="playerListingName" style={{backgroundColor: "#FFFFFF"}}>
-						<h1 className="playerListingName" style={{color: this.props.player.colour}}>
-							{this.props.player.screenName}
-						</h1>
-					</div>
-					<div className="playerListingDetails" style={{backgroundColor: "#FFFFFF"}}>
-						<h1 className={className} style={{color: this.props.player.colour}}>{scoreString}</h1>
-					</div>
-				</div>
-			);
+	textScale = (text, font, size, width) => {
+		const textWidth = this.textWidth(text, font, size);
+		if (textWidth <= width) {
+			return 1;
 		} else {
-			return (
-				<div className="playerListing">
-					<div className="playerListingName" style={{backgroundColor: this.props.player.colour}}>
-						<h1 className="playerListingName" style={{color: "#FFFFFF"}}>{this.props.player.screenName}</h1>
-					</div>
-					<div className="playerListingDetails" style={{backgroundColor: this.props.player.colour}}>
-						<h1 className={className} style={{color: "#FFFFFF"}}>{scoreString}</h1>
-					</div>
-				</div>
-			);
+			return width / textWidth;
 		}
-		
-		
+	}
+
+	textHorizontalSpacing = (text, font, size, width) => {
+		const textWidth = this.textWidth(text, font, size);
+		if (textWidth >= width) {
+			return 0;
+		} else {
+			return (width - textWidth)/2;
+		}
+	}
+
+	render = () => {
+		const nameWidthRatio = 0.8;
+		const {top, height, width, player, answering} = this.props;
+		const scoreString = player.score.toString();
+
+		const heightMargin = 0.1 * height;
+		const rectHeight = height - heightMargin * 2;
+		const widthMargin = 0.05 * width;
+		const rectWidth = width - widthMargin * 2;
+
+		const textHeightMargin = 0.1 * rectHeight;
+		const textHeight = rectHeight - textHeightMargin * 2;
+		const textWidthMargin = 0.05 * rectWidth;
+		const textWidth = rectWidth - textWidthMargin * 2;
+
+		const nameFont = "Verdana";
+		const scoreFont = "Impact";
+
+		const nameWidth = nameWidthRatio * rectWidth;
+		const nameTextWidth = textWidth * nameWidthRatio;
+		const nameTextWidthMargin = (nameWidth - nameTextWidth)/2;
+		const scoreWidth = (1 - nameWidthRatio) * rectWidth;
+		const scoreTextWidth = textWidth * (1-nameWidthRatio);
+		const scoreTextWidthMargin = (scoreWidth - scoreTextWidth)/2;
+
+		const offScreenX = -width;
+
+		return (
+			<Motion defaultStyle={{x: offScreenX, y: top + heightMargin}}
+				style={{x: spring(widthMargin), y: spring(top + heightMargin)}}>
+				{({x, y}) => (
+					<Group x={x} y={y} height={rectHeight} width={rectWidth}>
+						<Rect fill={answering ? "white" : player.colour} x={0} y={0} stroke="black" strokeWidth="1"
+							height={rectHeight} width={nameWidth} shadowColor="black"/>
+
+						<Rect fill={answering ? "white" : player.colour} x={nameWidthRatio * rectWidth} y={0} stroke="black" strokeWidth="1"
+							height={rectHeight} width={scoreWidth} shadowColor="black"/>
+
+						<Text x={nameTextWidthMargin + this.textHorizontalSpacing(player.screenName, nameFont, textHeight, nameTextWidth)}
+							y={textHeightMargin} height={textHeight} fontSize={textHeight} wrap="none"
+							scaleX={this.textScale(player.screenName, nameFont, textHeight, nameTextWidth)} align="left" shadowColor="black"
+							fontFamily={nameFont} fill={answering ? player.colour : "white"} text={player.screenName}/>
+
+						<Text x={nameWidth + scoreTextWidthMargin + this.textHorizontalSpacing(scoreString, scoreFont, textHeight, scoreTextWidth)}
+							y={textHeightMargin} height={textHeight} fontSize={textHeight} wrap="none"
+							scaleX={this.textScale(scoreString, scoreFont, textHeight, scoreTextWidth)} align="left" shadowColor="black"
+							fontFamily={scoreFont} fill={answering ? player.colour : "white"} text={scoreString}/>
+					</Group>
+				)}
+			</Motion>
+		);
 	}
 }
 
